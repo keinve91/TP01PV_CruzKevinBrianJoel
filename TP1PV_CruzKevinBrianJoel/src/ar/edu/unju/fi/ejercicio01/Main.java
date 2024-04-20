8fa9372036a18c908b14be9397968b5ec0ef8a26
Joel Cruz <Joel Cruz@DESKTOP-ITJELBR>
2024-04-19 23:32:20 -0300

    TRABAJO PRACTICO Nro1

--- TP1PV_CruzKevinBrianJoel/src/ar/edu/unju/fi/ejercicio01/Main.java
+++ TP1PV_CruzKevinBrianJoel/src/ar/edu/unju/fi/ejercicio01/Main.java
@@ -5,9 +5,9 @@ public class Main{
 			
 			int n1 = 8, n2 = 5, n3 = 1, n4 = 20, n5 = 9;
 			
-			float promedio = (n1+n2+n3+n4+n5) / 5.0f;
+			float resultado = (n1+n2+n3+n4+n5) / 5.0f;
 			
-			System.out.println("El promedio es: " + promedio);
+			System.out.println("El promedio es: " + resultado);
 			
 		}
 
